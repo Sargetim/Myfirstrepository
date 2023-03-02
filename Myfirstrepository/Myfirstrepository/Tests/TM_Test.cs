3c6402121ab129e07ef5ca93189a377f3bd9dd2e
saujan timala <timalasaujan@gmail.com>
2023-03-02 16:20:21 +1100

    Edited and Deleted Code

--- Myfirstrepository/Myfirstrepository/Tests/TM_Test.cs
+++ Myfirstrepository/Myfirstrepository/Tests/TM_Test.cs
@@ -50,10 +50,14 @@ IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"
 createNewButton.Click();
 Thread.Sleep(1000);
 
+
 // Select Time option from TypeCode dropdown list
 IWebElement typeCodeDropdown = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[2]/span"));
 typeCodeDropdown.Click();
-Thread.Sleep(1000);
+Thread.Sleep(6000);
+
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TypeCode_listbox\"]/li[2]"));
+timeOption.Click();
 
 // input code into Code textbook
 IWebElement codeTextbox = driver.FindElement(By.Id("Code"));
@@ -65,7 +69,7 @@ descriptionTextbox.SendKeys("First Repository");
 
 // Input Price per unit into price per unit textbox
 IWebElement priceTextbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
-priceTextbox.SendKeys("24");
+priceTextbox.SendKeys("12345");
 
 // Click on save button
 IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
@@ -86,6 +90,65 @@ if(newCode.Text == "First Repository")
 else
 {
     Console.WriteLine("Record hasn't been created");
+}
+
+// Click on edit button to edit record
+
+IWebElement editButton = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[1]"));
+editButton.Click();
+
+
+// Enter new record in Price field
+
+IWebElement codeTextBox = driver.FindElement(By.Id("Code"));
+codeTextBox.Clear();
+codeTextBox.SendKeys("MyFirstRepository1.0");
+
+//Click on save button after editing record
+IWebElement editsavebtn = driver.FindElement(By.Id("SaveButton"));
+editsavebtn.Click();
+Thread.Sleep(6000);
+
+
+
+// Go to last page after editing record
+
+IWebElement GoToLastPage1 = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+GoToLastPage1.Click();
+Thread.Sleep(5000);
+
+
+
+IWebElement editedCode = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+
+if (editedCode.Text == "MyFirstRepository1.0")
+{
+    Console.WriteLine("Record is edited successfully");
+}
+else
+{
+    Console.WriteLine("Record not Editted");
+}
+
+//Click on Delete button 
+IWebElement deletebtn = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[5]/a[2]"));
+deletebtn.Click();
+Thread.Sleep(6000);
+
+// Click on OK button on popup box
+IAlert al = driver.SwitchTo().Alert();
+al.Accept();
+Thread.Sleep(4000);
+
+
+IWebElement deleterecord = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if (deleterecord.Text == "First Repository 1.0")
+{
+    Console.WriteLine("Record has not been deleted successfully");
+}
+else
+{
+    Console.WriteLine("Record is deleted successfully");
 }
 
 
